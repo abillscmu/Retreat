6be228749bf3e6c8e57201b1e6ace6765d3b5dab
Alec Bills <abills@andrew.cmu.edu>
2022-04-06 17:58:13 -0400

    new algo

--- group_scheduling.jl
+++ group_scheduling.jl
@@ -16,6 +16,7 @@ n = length(people)
 model = Model(Gurobi.Optimizer)
 @variable(model,g_1[1:m-2],Bin)
 @variable(model,g_2[1:m-2],Bin)
+@variable(model,g_3[1:m-2],Bin)
 
 x = zeros(n,m-2)
 for i in 1:n
@@ -31,10 +32,14 @@ for i in 1:n
 end
 
 
-@objective(model,Min,sum(x*g_1)+sum(x*g_2))
-@constraint(model,[j in 1:m-2],g_1[j]+g_2[j]==1)
-@constraint(model,[i in 1:n],x[i,:]'*g_1>=1)
-@constraint(model,[i in 1:n],x[i,:]'*g_2>=1)
+@objective(model,Min,sum(x*g_1)^2+sum(x*g_2)^2+sum(x*g_3)^2)
+@constraint(model,[j in 1:m-2],g_1[j]+g_2[j]+g_3[j]==1)
+#@constraint(model,[i in 1:n],x[i,:]'*g_1>=1)
+#@constraint(model,[i in 1:n],x[i,:]'*g_2>=1)
 optimize!(model)
 
-    
+get_topics(g) = topics[findall(Bool.(value.(g))).+2]
+
+t_1 = get_topics(g_1)
+t_2 = get_topics(g_2)
+t_3 = get_topics(g_3)
